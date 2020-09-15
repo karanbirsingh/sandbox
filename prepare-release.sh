f921f16af2f4a6ad9dc2139fe00030b60fbe0434
Karan <karansin@microsoft.com>
2020-09-15 15:20:56 -0700

    Delete prepare-release.sh

--- prepare-release.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ -z ${1+x} ]; then
-    echo "No version version number passed as an argument to the script."
-    exit 1
-fi
-echo "Creating a release for version ${1}."
-export NEXTVERSION=${1}
